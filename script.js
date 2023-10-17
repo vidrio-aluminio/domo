a7b38e0d96b7a3dc5d78148c6356e34d82ee57ec
Edson Rubio <53164480+ERRL89@users.noreply.github.com>
2023-10-17 11:15:12 -0600

    Update script.js

--- script.js
+++ script.js
@@ -41,52 +41,52 @@ function imgURL(item) {
   console.log(item)
 
   if(item==1){
-    circulo1.src="../images/1.jpg"
-    circulo2.src="../images/2.jpg"
-    circulo3.src="../images/3.jpg"
-    circulo4.src="../images/4.jpg"
+    circulo1.src="./images/1.jpg"
+    circulo2.src="./images/2.jpg"
+    circulo3.src="./images/3.jpg"
+    circulo4.src="./images/4.jpg"
   }
 
   if(item==2){
-    circulo1.src="../images/cristal-templado/puertas/1.jpg"
-    circulo2.src="../images/cristal-templado/puertas/2.jpg"
-    circulo3.src="../images/cristal-templado/puertas/3.jpg"
-    circulo4.src="../images/cristal-templado/puertas/4.jpg"
+    circulo1.src="./images/cristal-templado/puertas/1.jpg"
+    circulo2.src="./images/cristal-templado/puertas/2.jpg"
+    circulo3.src="./images/cristal-templado/puertas/3.jpg"
+    circulo4.src="./images/cristal-templado/puertas/4.jpg"
   }
 
   if(item==3){
-    circulo1.src="../images/cristal-templado/ventanas/1.jpg"
-    circulo2.src="../images/cristal-templado/ventanas/2.jpg"
-    circulo3.src="../images/cristal-templado/ventanas/3.jpg"
-    circulo4.src="../images/cristal-templado/ventanas/4.jpg"
+    circulo1.src="./images/cristal-templado/ventanas/1.jpg"
+    circulo2.src="./images/cristal-templado/ventanas/2.jpg"
+    circulo3.src="./images/cristal-templado/ventanas/3.jpg"
+    circulo4.src="./images/cristal-templado/ventanas/4.jpg"
   }
 
   if(item==4){
-    circulo1.src="../images/cristal-templado/fachadas/1.jpeg"
-    circulo2.src="../images/cristal-templado/fachadas/2.jpeg"
-    circulo3.src="../images/cristal-templado/fachadas/3.jpeg"
-    circulo4.src="../images/cristal-templado/fachadas/4.jpeg"
+    circulo1.src="./images/cristal-templado/fachadas/1.jpeg"
+    circulo2.src="./images/cristal-templado/fachadas/2.jpeg"
+    circulo3.src="./images/cristal-templado/fachadas/3.jpeg"
+    circulo4.src="./images/cristal-templado/fachadas/4.jpeg"
   }
 
   if(item==5){
-    circulo1.src="../images/cristal-templado/mamparas/1.jpg"
-    circulo2.src="../images/cristal-templado/mamparas/2.jpg"
-    circulo3.src="../images/cristal-templado/mamparas/3.jpg"
-    circulo4.src="../images/cristal-templado/mamparas/4.jpg"
+    circulo1.src="./images/cristal-templado/mamparas/1.jpg"
+    circulo2.src="./images/cristal-templado/mamparas/2.jpg"
+    circulo3.src="./images/cristal-templado/mamparas/3.jpg"
+    circulo4.src="./images/cristal-templado/mamparas/4.jpg"
   }
 
   if(item==6){
-    circulo1.src="../images/cristal-templado/oficinas/1.jpeg"
-    circulo2.src="../images/cristal-templado/oficinas/2.jpeg"
-    circulo3.src="../images/cristal-templado/oficinas/3.jpeg"
-    circulo4.src="../images/cristal-templado/oficinas/4.jpeg"
+    circulo1.src="./images/cristal-templado/oficinas/1.jpeg"
+    circulo2.src="./images/cristal-templado/oficinas/2.jpeg"
+    circulo3.src="./images/cristal-templado/oficinas/3.jpeg"
+    circulo4.src="./images/cristal-templado/oficinas/4.jpeg"
   }
 
   if(item==7){
-    circulo1.src="../images/cristal-templado/repisas/1.jpg"
-    circulo2.src="../images/cristal-templado/repisas/2.jpg"
-    circulo3.src="../images/cristal-templado/repisas/3.jpg"
-    circulo4.src="../images/cristal-templado/repisas/4.jpg"
+    circulo1.src="./images/cristal-templado/repisas/1.jpg"
+    circulo2.src="./images/cristal-templado/repisas/2.jpg"
+    circulo3.src="./images/cristal-templado/repisas/3.jpg"
+    circulo4.src="./images/cristal-templado/repisas/4.jpg"
   }
 }
 
